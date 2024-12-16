5ad6b585bee4ab2accacc9d94dd431b29c7ca5de
christoph-sachse-sonarsource <christoph.sachse@sonarsource.com>
2024-12-16 18:59:47 +0000

    hrrr

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+        <hr/>  <hr/>  <hr/>  <hr/>  <hr/>  <hr/>  <hr/>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
